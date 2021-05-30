5e02dbe1e9ff7cd6e8549e66e18d8f2a8c7800aa
Rapid1898 <markuspolzer@gmx.at>
2021-05-30 16:11:33 +0200

    upd

--- date-fns/tmp.js
+++ date-fns/tmp.js
@@ -1,3 +1,5 @@
-import { format, compareAsc } from 'date-fns'
+const {format} = require("date-fns")
 
-format(new Date(2014, 1, 11), 'MM/dd/yyyy')
+console.log(format(new Date(2014, 1, 11), 'MM/dd/yyyy'))
+console.log(format(new Date(2014, 1, 11), 'yyyy-MM-dd'))
+console.log(typeof(format(new Date(2014, 1, 11), 'yyyy-MM-dd')))

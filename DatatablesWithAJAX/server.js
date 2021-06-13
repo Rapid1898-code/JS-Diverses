acec52af4d58e00b4ce777740d41d57ba1be6d49
Rapid1898 <rapid1898@gmail.com>
2021-06-13 16:52:57 +0200

    upd

--- DatatablesWithAJAX/server.js
+++ DatatablesWithAJAX/server.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const app = express();
-// const cors = require('cors')
-
 app.use(express.static('data'))
-// app.use(cors(builder => builder
-//   .AllowAnyOrigin()
-//   .AllowAnyMethod()
-//   .AllowAnyHeader()
-// ))
-
 app.listen(2121, () => {
   console.log("Server is running on port 2121...");
 });

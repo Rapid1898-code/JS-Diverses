742b7276f485d8607f6712dbdb1007ed6c9828bb
Rapid1898 <markuspolzer@gmx.at>
2021-06-14 14:50:38 +0200

    upd

--- DatatablesWithAJAX/data/mainAJAX.js
+++ DatatablesWithAJAX/data/mainAJAX.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   $('#example').DataTable( {
-      "ajax": "test.txt"
+      "ajax": "test.txt",
+      "columns": [
+        // { "data": "name" },
+        { "data": "ticker",
+          "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+            $(nTd).html("<a href='" + "https://www.orf.at" + "'>" + oData.ticker + "</a>");
+          }
+        },
+        { "data": "name" },
+        { "data": "calcDate" },
+        { "data": "index" },
+        { "data": "currency" },
+        { "data": "sector" },
+        { "data": "industry" },
+        { "data": "cap" },
+        { "data": "finStock" },
+        { "data": "score" },
+        { "data": "recommend" }
+    ]    
   } );
 } );
